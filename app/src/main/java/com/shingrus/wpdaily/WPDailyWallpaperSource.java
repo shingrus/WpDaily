c76dbd0e9decdbd42b3121a21a4adfc7a48788e9
shingrus <shingrus@gmail.com>
2016-01-23 18:57:22 +0300

    interface for the future wallpaper sources

---
+++ app/src/main/java/com/shingrus/wpdaily/WPDailyWallpaperSource.java
@@ -0,0 +1,14 @@
+package com.shingrus.wpdaily;
+
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Created by shingrus on 22/01/16.
+ * Interface for the abstract wallpaper source
+ */
+public interface WPDailyWallpaperSource {
+    String getWallpaperProvider();
+    URL GetLastWallpaperLink() throws IOException;
+
+}
