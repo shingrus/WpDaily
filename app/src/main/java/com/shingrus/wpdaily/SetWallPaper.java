8eb299f4d55dcfc286e3f74f7535de05990a6506
shingrus <shingrus@gmail.com>
2016-01-31 10:02:27 +0300

    set only one wall paper per day

--- app/src/main/java/com/shingrus/wpdaily/SetWallPaper.java
+++ app/src/main/java/com/shingrus/wpdaily/SetWallPaper.java
@@ -2,23 +2,19 @@ package com.shingrus.wpdaily;
 
 import android.app.WallpaperManager;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.preference.PreferenceManager;
 import android.util.Log;
-import android.util.Xml;
-
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
@@ -28,6 +24,7 @@ import java.util.Random;
  */
 public final class SetWallPaper {
 
+    public static final String WALLPAPER_UPDATE_TIME_KEY = "wallpaper_update_time";
     Context appContext;
     static SetWallPaper setWallPaper;
     static final String _log_tag = "SetWallPaper";
@@ -105,7 +102,7 @@ public final class SetWallPaper {
      * @param buffer - Buffer with image, that should be set as WP
      * @return true if success, false - overvise
      */
-    private boolean setWallPaperImage(byte[] buffer) {
+    public boolean setWallPaperImage(byte[] buffer) {
         boolean retVal = false;
         if (buffer != null) {
             WallpaperManager wp = WallpaperManager.getInstance(appContext);
@@ -113,6 +110,13 @@ public final class SetWallPaper {
                 Bitmap image = BitmapFactory.decodeByteArray(buffer, 0, buffer.length);
                 wp.setBitmap(image);
                 retVal = true;
+
+                SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(appContext);
+                SharedPreferences.Editor e = pref.edit();
+                Long tsLong = System.currentTimeMillis() / 1000;
+                e.putLong(WALLPAPER_UPDATE_TIME_KEY, tsLong);
+                e.apply();
+
             } catch (IOException e) {
                 Log.d(_log_tag, "set image error" + e);
             }
@@ -120,6 +124,14 @@ public final class SetWallPaper {
         return retVal;
     }
 
+    private boolean isMoreTimeElapsed(long seconds) {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(appContext);
+        long updateTime = pref.getLong(WALLPAPER_UPDATE_TIME_KEY, 0);
+        long now = System.currentTimeMillis() / 1000;
+        Log.d(_log_tag, "Check update time: " + updateTime + "vs" + now);
+        return (now - updateTime > seconds);
+
+    }
 
     /**
      * @param imageDescription - ImageDescription where to get  new image
@@ -134,13 +146,19 @@ public final class SetWallPaper {
 
                 byte[] imageBuf = getImage(url);
 
-                if (setWallPaperImage(imageBuf)) {
+                if (imageBuf.length > 0) {
+                    if (isMoreTimeElapsed(86400))
+                        setWallPaperImage(imageBuf);
+                    Log.d(_log_tag, "Store image but don't update wallpaper.");
+
                     Log.d(_log_tag, "Set new image:" + url);
 
                     storage.putImage(imageDescription.getUrl(), currentProvider.getWallpaperProvider(),
                             imageDescription.getLinkPage(), imageBuf);
                     retVal = UpdateResult.SUCCESS;
+
                 }
+
 
             } catch (MalformedURLException e) {
                 retVal = UpdateResult.PROVIDER_FAIL;
@@ -160,16 +178,15 @@ public final class SetWallPaper {
             //chose provider
             //need to keep last provider
             Random r = new Random();
-            for (int count = providers.size(), i = r.nextInt(count); count >0; count--,i++) {
-                currentProvider = providers.get(i%providers.size());
-                Log.d(_log_tag,"Check provider:" + currentProvider+ "; i:"+i+";count: "+count);
+            for (int count = providers.size(), i = r.nextInt(count); count > 0; count--, i++) {
+                currentProvider = providers.get(i % providers.size());
+                Log.d(_log_tag, "Check provider:" + currentProvider + "; i:" + i + ";count: " + count);
                 ImageDescription imageDescription = currentProvider.GetLastWallpaperLink();
                 if (imageDescription != null) {
                     retVal = setWallPaperImage(imageDescription);
-                    if (retVal==UpdateResult.SUCCESS || retVal == UpdateResult.NETWORK_FAIL)
+                    if (retVal == UpdateResult.SUCCESS || retVal == UpdateResult.NETWORK_FAIL)
                         break;
-                }
-                else {
+                } else {
                     retVal = UpdateResult.PROVIDER_FAIL;
                 }
             }
